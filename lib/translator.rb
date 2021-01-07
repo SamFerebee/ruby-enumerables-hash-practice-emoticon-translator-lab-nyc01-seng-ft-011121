4e011f27b771d990f8498184f80a0fb0a1b78d5a
Samuel Ferebee <sam.ferebee@gmail.com>
2021-01-07 17:44:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,20 +2,21 @@ require "yaml"
 require "pry"
 
 def load_library(path)
-  emoji_hash = {}
-  emoji_hash[:angel] = {:english => "O:)", :japanese => "☜(⌒▽⌒)☞"}
-  emoji_hash[:angry] = {:english =>">:(", :japanese =>"ヽ(ｏ`皿′ｏ)ﾉ"}
-  emoji_hash[:bored] = {:english => ":O", :japanese => "(ΘεΘ;)"}
-  emoji_hash[:confused] = {:english => "%)", :japanese => "(゜.゜)"}
-  emoji_hash[:embarrassed] = {:english => ":$" , :japanese => "(#^.^#)"}
-  emoji_hash[:fish] = {:english => "><>", :japanese =>"゜))))彡"}
-  emoji_hash[:glasses] = {:english => "8D", :japanese => "(^0_0^)"}
-  emoji_hash[:grinning] = {:english => "=D", :japanese => "(￣ー￣)"}
-  emoji_hash[:happy] = {:english =>":)", :japanese => "(＾ｖ＾)"}
-  emoji_hash[:kiss] = {:english =>   - ":*", :japanese => "(*^3^)/~☆"}
-  emoji_hash[:sad] = {:english =>":'(", :japanese => "(Ｔ▽Ｔ)"}
-  emoji_hash[:surprised] = {:english => ":o", :japanese => "o_O"}
-  emoji_hash[:wink] = {:english =>";)", :japanese => "(^_-)"}
+  emoji_hash = {
+    :angel => {:english => "O:)", :japanese => "☜(⌒▽⌒)☞"},
+    :angry => {:english =>">:(", :japanese =>"ヽ(ｏ`皿′ｏ)ﾉ"},
+    :bored => {:english => ":O", :japanese => "(ΘεΘ;)"},
+    :confused => {:english => "%)", :japanese => "(゜.゜)"},
+    :embarrassed => {:english => ":$" , :japanese => "(#^.^#)"},
+    :fish => {:english => "><>", :japanese =>"゜))))彡"},
+    :glasses => {:english => "8D", :japanese => "(^0_0^)"},
+    :grinning => {:english => "=D", :japanese => "(￣ー￣)"},
+    :happy => {:english =>":)", :japanese => "(＾ｖ＾)"},
+    :kiss => {:english =>   - ":*", :japanese => "(*^3^)/~☆"},
+    :sad => {:english =>":'(", :japanese => "(Ｔ▽Ｔ)"},
+    :surprised] = {:english => ":o", :japanese => "o_O"},
+    :wink] = {:english =>";)", :japanese => "(^_-)"}
+  }
   emoji_hash
 end
 
