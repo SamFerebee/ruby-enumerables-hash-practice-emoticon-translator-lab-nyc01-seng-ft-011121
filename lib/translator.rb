a550979361a9be319f762799e0d004042a3f1086
Samuel Ferebee <sam.ferebee@gmail.com>
2021-01-07 17:22:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,9 +4,9 @@ require "pry"
 def load_library(path)
   emoji_hash = {}
   emoji_hash[:angel] = {:enlish => "O:)", :japanese => "☜(⌒▽⌒)☞"}
-  emoji_hash[:angry] = {:english =>">:(" :japanese =>"ヽ(ｏ`皿′ｏ)ﾉ"}
-  emoji_hash[:bored] = {:english => ":O" :japanese => "(ΘεΘ;)"}
-  emoji_hash[:confused] = {}
+  emoji_hash[:angry] = {:english =>">:(", :japanese =>"ヽ(ｏ`皿′ｏ)ﾉ"}
+  emoji_hash[:bored] = {:english => ":O", :japanese => "(ΘεΘ;)"}
+  emoji_hash[:confused] = {:english => "%)", :japanese => "(゜.゜)"}
   emoji_hash[:embarrassed] = {}
   emoji_hash[:fish] = {}
   emoji_hash[:glasses] = {}
