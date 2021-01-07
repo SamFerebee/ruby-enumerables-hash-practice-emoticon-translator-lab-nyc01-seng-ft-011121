bb9f751fec7b86a7b634c506fcf86d52e2517025
Samuel Ferebee <sam.ferebee@gmail.com>
2021-01-07 17:52:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,19 +3,19 @@ require "pry"
 
 def load_library(path)
   emoji_hash = {
-    :angel => {:english => "O:)", :japanese => "☜(⌒▽⌒)☞"},
-    :angry => {:english =>">:(", :japanese =>"ヽ(ｏ`皿′ｏ)ﾉ"},
-    :bored => {:english => ":O", :japanese => "(ΘεΘ;)"},
-    :confused => {:english => "%)", :japanese => "(゜.゜)"},
-    :embarrassed => {:english => ":$" , :japanese => "(#^.^#)"},
-    :fish => {:english => "><>", :japanese =>"゜))))彡"},
-    :glasses => {:english => "8D", :japanese => "(^0_0^)"},
-    :grinning => {:english => "=D", :japanese => "(￣ー￣)"},
-    :happy => {:english =>":)", :japanese => "(＾ｖ＾)"},
-    :kiss => {:english =>   - ":*", :japanese => "(*^3^)/~☆"},
-    :sad => {:english =>":'(", :japanese => "(Ｔ▽Ｔ)"},
-    :surprised => {:english => ":o", :japanese => "o_O"},
-    :wink => {:english =>";)", :japanese => "(^_-)"}
+    "angel" => {:english => "O:)", :japanese => "☜(⌒▽⌒)☞"},
+    "angry" => {:english =>">:(", :japanese =>"ヽ(ｏ`皿′ｏ)ﾉ"},
+    "bored" => {:english => ":O", :japanese => "(ΘεΘ;)"},
+    "confused" => {:english => "%)", :japanese => "(゜.゜)"},
+    "embarrassed" => {:english => ":$" , :japanese => "(#^.^#)"},
+    "fish" => {:english => "><>", :japanese =>"゜))))彡"},
+    "glasses" => {:english => "8D", :japanese => "(^0_0^)"},
+    "grinning" => {:english => "=D", :japanese => "(￣ー￣)"},
+    "happy" => {:english =>":)", :japanese => "(＾ｖ＾)"},
+    "kiss" => {:english =>   - ":*", :japanese => "(*^3^)/~☆"},
+    "sad" => {:english =>":'(", :japanese => "(Ｔ▽Ｔ)"},
+    "surprised" => {:english => ":o", :japanese => "o_O"},
+    "wink" => {:english =>";)", :japanese => "(^_-)"}
   }
   emoji_hash
 end
