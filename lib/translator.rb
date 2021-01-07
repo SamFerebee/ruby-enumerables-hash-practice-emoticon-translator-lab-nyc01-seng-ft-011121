997da87af1ee9b27052e7b28c6c5c7101c8699e7
Samuel Ferebee <sam.ferebee@gmail.com>
2021-01-07 17:24:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(path)
   emoji_hash[:confused] = {:english => "%)", :japanese => "(゜.゜)"}
   emoji_hash[:embarrassed] = {:english => "%)", :japanese => "(゜.゜)"}
   emoji_hash[:fish] = {:english => "><>", :japanese =>">゜))))彡"}
-  emoji_hash[:glasses] = {}
+  emoji_hash[:glasses] = {:english => "8D", :japanese => "(^0_0^)"}
   emoji_hash[:grinning] = {}
   emoji_hash[:happy] = {}
   emoji_hash[:kiss] = {}
